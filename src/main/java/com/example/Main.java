60e6d4acef30d9ea127a239b8b8b9f5016f733aa
joseleonardosg19-ship-it <joseleonardosg19@gmail.com>
2025-11-08 16:51:47 -0500

    Proyecto integrado

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,35 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Autenticacion();
+    }
+
+    public static void Autenticacion() {
+        Scanner scanner = new Scanner(System.in);
+
+        String usuario = "educlic@gmail.com";
+        String password = "t4nia";
+
+        while (true) {
+        System.out.println("Ingresa tu correo electronico ");
+        usuario = scanner.nextLine();
+
+        System.out.println("Ingresa tu contraseña ");
+        password = scanner.nextLine();
+
+        if (usuario.equals("educlic@gmail.com") && password.equals("t4nia")) {
+          
+          System.out.println("Sesion iniciada");
+          break;
+          
+          
+          } else {
+          System.out.println("Usuario o contraseña incorrecta");
+          }  
+        }
     }
 }
+    
